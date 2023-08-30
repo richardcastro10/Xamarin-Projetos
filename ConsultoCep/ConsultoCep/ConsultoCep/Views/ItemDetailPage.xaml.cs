eff1c94184d62f8cbdfa3e8bd4c73a972352a0d4
Richard <Richard@DESKTOP-UA2S0EV>
2023-08-29 23:11:45 -0300

    Adicionar arquivos de projeto.

---
+++ ConsultoCep/ConsultoCep/ConsultoCep/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using ConsultoCep.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace ConsultoCep.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
